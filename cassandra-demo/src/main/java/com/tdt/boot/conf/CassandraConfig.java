f83404df24c3a479ae6acf12c5ab0d19a181be33
liarboss <124275630@qq.com>
2022-05-30 15:25:32 +0800

    update

--- cassandra-demo/src/main/java/com/tdt/boot/conf/CassandraConfig.java
+++ cassandra-demo/src/main/java/com/tdt/boot/conf/CassandraConfig.java
@@ -1,17 +1,11 @@
 package com.tdt.boot.conf;
 
-import com.datastax.oss.driver.api.core.CqlSession;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.autoconfigure.cassandra.CassandraProperties;
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.cassandra.config.AbstractCassandraConfiguration;
 import org.springframework.data.cassandra.config.CqlSessionFactoryBean;
-
-import java.net.InetSocketAddress;
-import java.util.List;
-import java.util.stream.Collectors;
 
 @Configuration
 public class CassandraConfig extends AbstractCassandraConfiguration {
